b4bb6aeb927214d3dd023c01a9da49a92a1f44cf
neno-kreso <107863186+neno-kreso@users.noreply.github.com>
2022-06-20 15:21:29 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -26,8 +26,8 @@ while True:
 
         # read and print out humidity and temperature from sensor
 
-        humidity = float(bme280.get_humidity())
-        temp = float(bme280.get_temperature())
+        humidity = bme280.get_humidity()
+        temp = bme280.get_temperature()
         print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp,humidity))
 
         # ensure that timestamp string is formatted properly
@@ -36,7 +36,7 @@ while True:
 
         # data that we're sending to Power BI REST API
 
-        data = '[{{ "temperature": "{1:0.1f}", "humidity": "{2:0.1f}", "timestamp": "{0}" }}]'.format(temp,humidity,now)
+        data = '[{{ "temperature": "{1:0.1f}", "humidity": "{2:0.1f}", "timestamp": "{0}" }}]'.format(float(temp),float(humidity),now)
 
         # make HTTP POST request to Power BI REST API
 
