d69f2fb423c11cf137897eae31ada48241584a6c
neno-kreso <107863186+neno-kreso@users.noreply.github.com>
2022-06-20 15:49:42 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import urllib
-import urllib.request as request
-import urllib.response as response
-import time
+import urllib2 as request
+import time 
 from datetime import datetime
 from bme280 import BME280
 
