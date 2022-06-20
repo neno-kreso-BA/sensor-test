352d10a519c3536967f313ccf5d9adeaf832e3c1
neno-kreso <107863186+neno-kreso@users.noreply.github.com>
2022-06-20 14:49:01 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -18,8 +18,7 @@ bme280 = BME280(i2c_dev=bus)
 # REST API endpoint, given to you when you create an API streaming dataset
 # Will be of the format: https://api.powerbi.com/beta/<tenant id>/datasets/< dataset id>/rows?key=<key id>
 
-REST_API_URL = \
-    ' https://api.powerbi.com/beta/ddd66cce-ffe1-4029-967c-5e15ef73127f/datasets/68c21b77-d940-4815-ade3-cf2f56c3895a/rows?key=eFN8uasQB8lLiqo8b95qUC9XXkzF2ilqHFHEa7JOEotKMuQpecfoNXHPcHvSGwv4DLHNS5t%2FqwbJQ5YlnaD9yw%3D%3D '
+REST_API_URL = ' https://api.powerbi.com/beta/ddd66cce-ffe1-4029-967c-5e15ef73127f/datasets/68c21b77-d940-4815-ade3-cf2f56c3895a/rows?key=eFN8uasQB8lLiqo8b95qUC9XXkzF2ilqHFHEa7JOEotKMuQpecfoNXHPcHvSGwv4DLHNS5t%2FqwbJQ5YlnaD9yw%3D%3D '
 
 while True:
     try:
@@ -28,8 +27,7 @@ while True:
 
         humidity = bme280.get_humidity()
         temp = bme280.get_temperature()
-        print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp,
-                humidity)
+        print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp,humidity)
 
         # ensure that timestamp string is formatted properly
 
@@ -37,17 +35,14 @@ while True:
 
         # data that we're sending to Power BI REST API
 
-        data = \
-            '[{{ "temperature": "{1:0.1f}", "humidity": "{2:0.1f}", "timestamp": "{0}" }}]'.format(now,
-                temp, humidity)
+        data = '[{{ "temperature": "{1:0.1f}", "humidity": "{2:0.1f}", "timestamp": "{0}" }}]'.format(now,temp, humidity)
 
         # make HTTP POST request to Power BI REST API
 
         req = urllib2.Request(REST_API_URL, data)
         response = urllib2.urlopen(req)
         print 'POST request to Power BI with data:{0}'.format(data)
-        print 'Response: HTTP {0} {1}\n'.format(response.getcode(),
-                response.read())
+        print 'Response: HTTP {0} {1}\n'.format(response.getcode(),response.read())
 
         time.sleep(1)
     except urllib2.HTTPError, e:
