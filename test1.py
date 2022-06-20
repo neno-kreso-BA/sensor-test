9f5cec8ad2757310da271adc2db4535ffb738741
neno-kreso <107863186+neno-kreso@users.noreply.github.com>
2022-06-20 14:28:45 +0100

    test1.py

---
+++ test1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import urllib, urllib2, time
+from datetime import datetime
+from bme280 import BME280
+
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+    
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+
+# REST API endpoint, given to you when you create an API streaming dataset
+# Will be of the format: https://api.powerbi.com/beta/<tenant id>/datasets/< dataset id>/rows?key=<key id>
+REST_API_URL = " *** Your Push API URL goes here *** "
+
+while True:
+	try:
+		# read and print out humidity and temperature from sensor
+		humidity = bme280.get_humidity()
+    temp = bme280.get_temperature()
+		print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp, humidity)
+		
+		# ensure that timestamp string is formatted properly
+		now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S%Z")
+	
+		# data that we're sending to Power BI REST API
+		data = '[{{ "timestamp": "{0}", "temperature": "{1:0.1f}", "humidity": "{2:0.1f}" }}]'.format(now, temp, humidity)
+	
+		# make HTTP POST request to Power BI REST API
+		req = urllib2.Request(REST_API_URL, data)
+		response = urllib2.urlopen(req)
+		print("POST request to Power BI with data:{0}".format(data))
+		print("Response: HTTP {0} {1}\n".format(response.getcode(), response.read()))	
+	
+		time.sleep(1)
+  except urllib2.HTTPError as e:
+		print("HTTP Error: {0} - {1}".format(e.code, e.reason))
+	except urllib2.URLError as e:
+		print("URL Error: {0}".format(e.reason))
+	except Exception as e:
+		print("General Exception: {0}".format(e))
