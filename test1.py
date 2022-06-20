a61e4971a16d5c0223263c435b775fd26a7b7a37
neno-kreso <107863186+neno-kreso@users.noreply.github.com>
2022-06-20 18:05:28 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -57,7 +57,7 @@ while True:
         print('POST request to Power BI with data:{0}'.format(data))
         print('Response: HTTP {0} {1}\n'.format(response.getcode(),response.read()))
         
-        time.sleep(2.5)
+        time.sleep(10)
         
     except request.HTTPError as e:
         print('HTTP Error: {0} - {1}'.format(e.code, e.reason))
