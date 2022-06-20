7510747e16dad97dd346ef9e1703b8f95dc848a7
neno-kreso <107863186+neno-kreso@users.noreply.github.com>
2022-06-20 15:10:37 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -39,8 +39,8 @@ while True:
 
         # make HTTP POST request to Power BI REST API
 
-        #req = urllib.Request(REST_API_URL, data)
-        response = urllib.request.urlopen(REST_API_URL, data)
+        req = urllib.request.urlopen(REST_API_URL, data)
+        response = urllib.request.urlopen(req)
         print('POST request to Power BI with data:{0}'.format(data))
         print('Response: HTTP {0} {1}\n'.format(response.getcode(),response.read()))
 
